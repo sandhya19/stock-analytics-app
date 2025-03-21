65876f3fceb168cf74ff9e64af208a56fac54732
Sandhya <sand.ash19@gmail.com>
2025-03-21 13:58:26 +0000

    Add requests layer to lambda function

--- main.tf
+++ main.tf
@@ -1,3 +1,43 @@
 provider "aws" {
   region = "eu-west-2"
 }
+
+data "archive_file" "lambda_package" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/main.py"
+  output_path = "${path.module}/lambda_function_payload.zip"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_lambda_function" "stock_fetcher" {
+  function_name    = "stockFetcher"
+  filename         = data.archive_file.lambda_package.output_path
+  source_code_hash = data.archive_file.lambda_package.output_base64sha256
+  handler          = "main.lambda_handler"
+  runtime          = "python3.9"
+  role             = aws_iam_role.lambda_exec_role.arn
+  
+  layers = [
+    "arn:aws:lambda:eu-west-2:396608782131:layer:requests-layer:1"
+  ]
+
+  environment {
+    variables = {
+      ALPHA_VANTAGE_API_KEY = var.alpha_vantage_api_key
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "alpha_vantage_api_key" {
+  description = "API key for Alpha Vantage"
+  type        = string
+  sensitive   = true
+}

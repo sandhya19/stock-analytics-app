2fb29539358446d1d1d742a1259ebf8fc7ebacee
Sandhya <sand.ash19@gmail.com>
2025-03-23 14:17:12 +0000

    Add CDN for the stack

--- infra.tf
+++ infra.tf
@@ -214,3 +214,58 @@ resource "aws_s3_object" "index" {
 output "frontend_website_url" {
   value = aws_s3_bucket.frontend.website_endpoint
 }
+
+resource "aws_cloudfront_distribution" "frontend_cdn" {
+  origin {
+    domain_name = aws_s3_bucket.frontend.website_endpoint
+    origin_id   = "s3Origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3Origin"
+
+    forwarded_values {
+      query_string = true
+      headers      = ["Origin"]
+    
+    cookies {
+    forward = "none" 
+    }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true  # Free SSL!
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "Frontend CDN"
+  }
+}
+
+output "cdn_url" {
+  value = aws_cloudfront_distribution.frontend_cdn.domain_name
+}
